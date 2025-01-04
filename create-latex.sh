3dc3a53017173fb0c300accb0b19729ce4307341
AHMZ <ahmz.ut@gmail.com>
2025-01-04 18:42:05 +0330

    Update Create Latex Script

--- create-latex.sh
+++ create-latex.sh
@@ -9,20 +9,19 @@ fi
 PROJECT_NAME=$1
 INSTALL_DIR="$HOME/latex_utils"
 TEMPLATE_DIR="$INSTALL_DIR/sample"
-UTILS_DIR="$INSTALL_DIR/utils"
 DEST_DIR="$PWD/$PROJECT_NAME"
 
 # Create project directory
 mkdir -p "$DEST_DIR"
 
 # Copy main.tex from template
-cp "$TEMPLATE_DIR/*" "$DEST_DIR/"
+cp -r "$TEMPLATE_DIR/"* "$DEST_DIR/"
 
 # remove already existing utils directory
 rm -rf "$DEST_DIR/utils"
 
 # Create symbolic link to utils directory
-ln -s "$UTILS_DIR" "$DEST_DIR/utils"
+ln -s "$INSTALL_DIR" "$DEST_DIR/utils"
  
 echo "LaTeX project '$PROJECT_NAME' created successfully."
 echo "Navigate to the project directory and start working on your LaTeX document."
